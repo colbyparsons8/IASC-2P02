7541fb48b7083bb1c88f8723ce57ac061a113681
colbyparsons8 <>
2025-02-02 20:13:03 -0500

    Add Prototype 2

--- prototype2/script.js
+++ prototype2/script.js
@@ -12,7 +12,7 @@ import * as dat from "lil-gui"
     width : window.innerWidth,
     height : window.innerHeight,
     aspectRatio : window.innerWidth / window.innerHeight
- }
+}
 
 
 
@@ -35,7 +35,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 )
 Scene.add(camera)
-camera.position.set(0,0,5)
+camera.position.set(-2,3,-5)
 
 //Renderer
 const Renderer = new THREE.WebGLRenderer({
@@ -54,12 +54,25 @@ controls.enableDamping = true
 ** MESHES **
 ***********/
 
-//testsphere
-const sphereGeometry = new THREE.SphereGeometry(1)
-const sphereMaterial = new THREE.MeshNormalMaterial()
-const testSphere =new THREE.Mesh(sphereGeometry, sphereMaterial)
+//box
+const boxGeometry = new THREE.BoxGeometry()
+const boxMaterial = new THREE.MeshNormalMaterial()
+const box =new THREE.Mesh(boxGeometry, boxMaterial)
 
-Scene.add(testSphere)
+Scene.add(box)
+
+//Plane
+const planeGeometry= new THREE.PlaneGeometry(10,10,50,50)
+const planeMaterial= new THREE.MeshBasicMaterial({
+    color: new THREE.Color('blue'),
+    side: THREE.DoubleSide,
+    wireframe: true
+
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+plane.rotation.x = Math.PI * 0.5
+
+Scene.add(plane)
 
 
 /*******
@@ -68,6 +81,38 @@ Scene.add(testSphere)
 
 //UI
 const ui = new dat.GUI()
+
+//UI Object
+const uiObject = {
+    speed: 1,
+    distance: 1,
+}
+
+//Plane UI
+const planeFolder = ui.addFolder ('Plane')
+
+planeFolder
+    .add(planeMaterial, 'wireframe')
+    .name("Toggle Wireframe")
+
+//testSphere UI
+const boxFolder = ui.addFolder( 'BOX' )
+
+boxFolder
+    .add(uiObject, 'speed')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Speed')
+
+boxFolder
+    .add(uiObject, 'distance')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Distance')
+
+
 
 
 
@@ -80,6 +125,10 @@ const animation = () =>
 {
     //Return elapsed time
     const elapsedTime = clock.getElapsedTime()
+
+    //Anitmate ring
+    box.position.y =Math.sin(elapsedTime * uiObject.speed) * uiObject.distance
+    box.position.x = Math.sin(elapsedTime)
     
     // Update OrbitControls
     controls.update()
