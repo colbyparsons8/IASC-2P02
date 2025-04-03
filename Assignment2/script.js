199f0630a9e7bf10026898d6c72f51e0d91120b9
colbyparsons8 <>
2025-04-03 06:10:30 -0400

    finnish assignment 2

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -77,40 +77,82 @@ Scene.add(directionalLight)
 ***********/
 
 //testsphere
-// Cube Geometry
-const cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+// sphere Geometry
+const sphereGeometry = new THREE.SphereGeometry(0.5, 0.5, 0.5)
 
-const drawCube = (height, params) =>
+const drawSphere = (height, params) =>
 {
-    //Create cub Materail
-    const material = new THREE.MeshStandardMaterial({
-        color: new THREE.Color(params.color)
-    })
+    //Create sphere Materail
+    let material
+    if(params.emissive)
+    {
+        material =new THREE.MeshLambertMaterial({
+            emissive: new THREE.Color(params.color),
+            emissiveIntensity: 200
+        })
+    
+    }
+    if(params.toon)
+    {
+        material = new THREE.MeshToonMaterial({
+            color: new THREE.Color(params.color)
+        })
+    }
+    if(params.basic)
+    {
+        material = new THREE.MeshBasicMaterial({
+            color: new THREE.Color(params.color)
+        })
+    }
+    
+    else 
+    {
+          material = new THREE.MeshStandardMaterial({
+             color: new THREE.Color(params.color)
+        })
+    }
+
+
+    //WireFrame Material
+    if(params.wireframe)
+    {
+        material.wireframe = true
+    }
 
     //create cube
-    const cube = new THREE.Mesh(cubeGeometry, material)
+    const sphere = new THREE.Mesh(sphereGeometry, material)
 
     // Position cube
-    cube.position.x = (Math.random()- 0.5)*params.diameter
-    cube.position.z = (Math.random()- 0.5)*params.diameter
-    cube.position.y = height - 10
+    sphere.position.x = (Math.random()- 0.5)*params.diameter
+    sphere.position.z = (Math.random()- 0.5)*params.diameter
+    sphere.position.y = height - 10
 
     //Scale Cube
-    cube.scale.x = params.scale
-    cube.scale.y = params.scale
-    cube.scale.z = params.scale
+    sphere.scale.x = params.scale
+    sphere.scale.y = params.scale
+    sphere.scale.z = params.scale
+
+
+    //Dynamic Scale
+    const elapsedTime= clock.getElapsedTime();
+    if(params.dynamicScale)
+    {
+        sphere.scale.x = height *0.2
+        sphere.scale.y = height *0.2
+        sphere.scale.z = height *0.2
+    }
 
     //randomize cube roation
     if(params.randomize){
-        cube.rotation.x = Math.random() *2 *Math.PI
-        cube.rotation.z = Math.random() *2 *Math.PI
-        cube.rotation.y = Math.random() *2 *Math.PI
+        sphere.rotation.x = Math.random() *2 *Math.PI
+        sphere.rotation.z = Math.random() *2 *Math.PI
+        sphere.rotation.y = Math.random() *2 *Math.PI
     }
     
 
 
     //add cube to group
-    params.group.add(cube)
+    params.group.add(sphere)
 }
 
 
@@ -139,39 +181,72 @@ const group2 =new THREE.Group()
 Scene.add(group2)
 const group3 =new THREE.Group()
 Scene.add(group3)
+const group4 =new THREE.Group()
+Scene.add(group4)
+
 
 const uiObj = {
-    sourceText: "The quick brown fox jumped over the lazy dog",
+    sourceText: "",
     saveSourceText() {
         saveSourceText()
     },
     term1: {
-        term: 'fox',
-        color: '#aa0ff',
-        diameter: 10,
+        term: 'adhd',
+        basic: true,
+        color: 'white',
+        diameter: 2,
+        dynamicScale: false,
+        emissive: true,
         group: group1,
-        nCubes: 100,
+        nSpheres: 20,
+        toon: false,
         randomize: true,
-        scale: 1
+        scale: 2,
+        wireframe: false
         
     },
     term2: {
-        term: 'dog',
-        color: '00ffaa',
-        diameter: 10,
+        term: 'animation',
+        basic: false,
+        color: 'green',
+        diameter: 7,
+        dynamicScale: true,
+        emissive: false,
         group: group2,
-        nCubes: 100,
+        nSpheres: 100,
+        toon: true,
         randomize: true,
-        scale: 1
+        scale: 1,
+        spins: true,
+        wireframe: false
     },
     term3: {
-        term: '',
-        color: '',
-        diameter: 10,
+        term: 'conventional',
+        basic: false,
+        color: 'red',
+        diameter: 5,
+        dynamicScale: false,
+        emissive: true,
         group: group3,
-        nCubes: 100,
+        nSpheres: 100,
+        toon: false,
+        randomize: false,
+        scale: 1,
+        wireframe: false
+    },
+    term4: {
+        term: 'academic',
+        basic: false,
+        color: 'yellow',
+        diameter: 12,
+        dynamicScale: false,
+        emissive: false,
+        group: group4,
+        nSpheres: 20,
+        toon: false,
         randomize: true,
-        scale: 1
+        scale: 1,
+        wireframe: true
     },
     saveTerms() {
         saveTerms()
@@ -179,6 +254,9 @@ const uiObj = {
     rotateCamera:false
 
 }
+
+//other words
+// Conventional, cognitve, instruction, animation, performance
 
 //ui function
 
@@ -214,6 +292,7 @@ const saveTerms = () =>
     findSearchTermInTokenizedText(uiObj.term1)
     findSearchTermInTokenizedText(uiObj.term2)
     findSearchTermInTokenizedText(uiObj.term3)
+    findSearchTermInTokenizedText(uiObj.term4)
 }
 
 
@@ -271,6 +350,20 @@ termsFolder
     .addColor(uiObj.term3, 'color')
     .name("Term 3 Color")
 
+termsFolder
+    .add(uiObj.term4, 'term')
+    .name("Term 4")
+
+termsFolder
+    .add(group4, 'visible')
+    .name("Term 4 Visibility")
+
+
+termsFolder
+    .addColor(uiObj.term4, 'color')
+    .name("Term 4 Color")
+
+
 visualizeFolder
     .add(uiObj, 'saveTerms')
     .name("Visualize")
@@ -319,9 +412,9 @@ const findSearchTermInTokenizedText = (params) =>
             const height = (100/ tokenizedText.length) *i*0.2
 
             //call drawcube nCubes using height value
-            for(let a =0; a< params.nCubes; a++)
+            for(let a =0; a< params.nSpheres; a++)
             {
-                drawCube(height, params)
+                drawSphere(height, params)
             }
             
         }
@@ -347,6 +440,14 @@ const animation = () =>
     // Update OrbitControls
     controls.update()
 
+    //rotate group 2
+    group2.rotation.y = elapsedTime
+
+    //spin group 3
+    group3.position.x = Math.sin(elapsedTime*0.2) *10
+    group3.position.z = Math.sin(elapsedTime*0.2) *10
+
+
     //Rotate Camera
     if(uiObj.rotateCamera)
     {
@@ -355,6 +456,7 @@ const animation = () =>
         camera.position.y = 5
         camera.lookAt(0,0,0)
     }
+
 
     //Renderer
     Renderer.render(Scene, camera)
